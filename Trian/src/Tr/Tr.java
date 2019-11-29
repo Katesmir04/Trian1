1f5f31e1cb89caaa4854e82f793453742bbb89e2
Katesmir04 <50634264+Katesmir04@users.noreply.github.com>
2019-11-29 15:23:16 +0300

    Add files via upload

---
+++ Trian/src/Tr/Tr.java
@@ -0,0 +1,13 @@
+package Tr;
+
+import tr.Triangle;
+
+public class Tr {
+    public static void main(String[] args)
+    {
+        new Triangle().Triangle1(8, 6 ,5);
+        new Triangle().Triangle1(7,6,5);
+        new Triangle().Triangle1(3,4,5);
+        new Triangle().Triangle1(7,4,3);
+    };
+}
